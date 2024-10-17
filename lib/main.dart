9469eb2a3ba88f9ee3a69aa2c68cbe5fc80c8e27
Kikegg97 <tyerryenry@gmail.com>
2024-10-17 02:18:50 -0500

    Added functionality to fetch and display commit history from GitHub API

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -27,15 +29,45 @@ class CommitListPage extends StatefulWidget {
 }
 
 class _CommitListPageState extends State<CommitListPage> {
+  List commits = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCommits();
+  }
+
+  Future<void> fetchCommits() async {
+    final response = await http.get(
+      Uri.parse('https://api.github.com/repos/Kikegg97/github-commits-viewer/commits'),
+    );
+
+    if (response.statusCode == 200) {
+      setState(() {
+        commits = json.decode(response.body);
+      });
+    } else {
+      throw Exception('Failed to load commits');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Commit History'),
       ),
-      body: const Center(
-        child: Text('Loading commits...'),
-      ),
+      body: commits.isEmpty
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: commits.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(commits[index]['commit']['message']),
+                  subtitle: Text(commits[index]['commit']['author']['name']),
+                );
+              },
+            ),
     );
   }
 }
